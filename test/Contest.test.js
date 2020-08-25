7ffce9693fd4a40d789b18ed5d4f041b6f1f4611
raajtilaksarma <raajtilaksarma@gmail.com>
2020-08-26 03:12:26 +0530

    add mocha test

---
+++ test/Contest.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile');
+
+let contest;
+let accounts;
+
+// use ganache to create mock account
+// send mock transactions
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    contest = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data : bytecode})
+    .send({from: accounts[0], gas: '1000000'});
+});
+
+describe('LuckyDraw Contract', () => {
+    it('can deploy a contract', () => {
+        assert.ok(contest.options.address);
+    });
+});
